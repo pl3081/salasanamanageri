c5a36bfeb2c11c83e73861ace96a87705777d4d4
pl3081 <petteri.j.lehto@gmail.com>
2022-04-19 13:14:36 +0300

    login computerchange 19.4.22
    vaihdan läppäriltä pöytäkoneelle

---
+++ login.py
@@ -0,0 +1,43 @@
+from tkinter import * 
+import tkinter as tk
+from cryptography.fernet import Fernet
+from registration import Register
+
+root = Tk()
+
+
+base_text = tk.Label(text="Kirjaudu sisään")
+base_text.grid(row=0, column=0)
+
+username_text = tk.Label(text="Käyttäjänimi")
+username_text.grid(row=1, column=0)
+
+username_entry = tk.Entry()
+username_entry.grid(row=2, column=0)
+
+password_text = tk.Label(text="salasana")
+password_text.grid(row=3, column=0)
+
+password_entry = tk.Entry()
+password_entry.grid(row=4, column=0)
+
+
+def login_button():
+    pass
+
+def registration_button():
+    root.destroy()
+    Register.Main()
+    
+    
+    
+
+
+login_button = tk.Button(text="login", command=login_button)
+login_button.grid(row=4,column=1)
+
+registation_button = tk.Button(text="Rekisteröidy", command=registration_button)
+registation_button.grid(row=5, column=0)
+
+    
+root.mainloop()
---
+++ registration.py
@@ -0,0 +1,50 @@
+from cryptography.fernet import Fernet
+import tkinter as tk
+from tkinter import *
+import os
+
+from matplotlib.style import use
+
+
+
+
+
+
+class Register():
+    
+
+    
+    
+    def Main():
+        def registratration():
+            parent_dir = "/salasanamanageri/login_details"
+            username = username_entry.get()
+            print(username)
+            print(type(parent_dir))
+            path = os.path.join(parent_dir, username)
+            os.mkdir(path=parent_dir, username) 
+            
+        
+
+
+        reg_root = Tk()
+        base_text = tk.Label(text="Rekisteröidy")
+        base_text.grid(row=0, column=0)
+
+        username_text = tk.Label(text="Käyttäjänimi")
+        username_text.grid(row=1, column=0)
+
+        username_entry = tk.Entry()
+        username_entry.grid(row=2, column=0)
+
+        password_text = tk.Label(text="salasana")
+        password_text.grid(row=3, column=0)
+
+        password_entry = tk.Entry()
+        password_entry.grid(row=4, column=0)
+
+        registration_button = tk.Button(text="rekisteröidy", command=registratration)
+        registration_button.grid(row=4,column=1)
+        reg_root.mainloop()
+
+    
