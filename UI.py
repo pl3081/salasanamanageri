db218238460a0d1d7fbf414b653d8bc5cbc6a1b0
DonnieAK <donnie.kanerva@gmail.com>
2022-05-10 13:22:18 +0300

    UI.py
    Buttons?

--- UI.py
+++ UI.py
@@ -1,4 +1,19 @@
-import tkinter
-top = tkinter.Tk()
+import tkinter as tk
+from tkinter import *
 
-top.mainloop()
+root = tk.Tk()
+root.geometry('1280x720')
+root.title('Password manager UI')
+
+# Buttons
+button1=tk.Button(root, text="button1")
+button1.grid(row=0,column=0)
+
+button2=tk.Button(root, text="button2")
+button2.grid(row=1,column=0)
+
+# Passwords
+
+# Websites
+
+root.mainloop()
