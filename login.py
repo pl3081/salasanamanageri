d3a3a7f7b1683ada85d3135dd48aa03dc92bf94d
pl3081 <petteri.j.lehto@gmail.com>
2022-05-10 13:36:10 +0300

    login and registration functional

---
+++ login.py
@@ -0,0 +1,64 @@
+from tkinter import * 
+import tkinter as tk
+from cryptography.fernet import Fernet
+from registration import Register
+import hashlib
+import os
+
+
+
+
+
+root = Tk()
+
+
+base_text = tk.Label(text="Kirjaudu sisään")
+base_text.grid(row=0, column=0)
+
+username_text = tk.Label(text="Käyttäjänimi")
+username_text.grid(row=1, column=0)
+
+username_entry = tk.Entry()
+username_entry.grid(row=2, column=0)
+
+password_text = tk.Label(text="salasana")
+password_text.grid(row=3, column=0)
+
+password_entry = tk.Entry()
+password_entry.grid(row=4, column=0)
+
+
+def login_button():
+    get_username = username_entry.get()
+    get_password = password_entry.get().encode()
+    encrypted_password = hashlib.sha256(get_password).hexdigest()
+    parent_directory = "../salasanamanageri/login_details"
+    password_directory = get_username + "/" + "password"
+    path = os.path.join(parent_directory, password_directory)
+    try:
+        with open(path + "/" + "password.txt", 'r') as f:
+            data = f.readline()
+            f.close()
+        if encrypted_password == data:
+            print("kirjautuminen onnistui")
+        else:
+            print("syöttämäsi salasana ei ole oikea")
+    except:
+        print("kirjoittamaasi käyttäjänimeä ei ole olemassa")
+
+def registration_button():
+    root.destroy()
+    Register.Main()
+    
+    
+    
+
+
+login_button = tk.Button(text="login", command=login_button)
+login_button.grid(row=4,column=1)
+
+registation_button = tk.Button(text="Rekisteröidy", command=registration_button)
+registation_button.grid(row=5, column=0)
+
+    
+root.mainloop()
