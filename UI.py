7b1d143147a3f95691e1f727b27f4287ad36cda7
DonnieAK <donnie.kanerva@gmail.com>
2022-05-17 00:26:50 +0300

    UI.py
    Pari turhaa juttua pois

--- UI.py
+++ UI.py
@@ -1,19 +1,19 @@
 import tkinter as tk
 from tkinter import *
 
-class Example(tk.Frame):
+class PassManager(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
         self.buttons = []
-        for i in range(6):
+        for i in range(4):
             self.buttons.append(tk.Button(self, text="Button %s" % (i+1,), background="gray"))
-        self.main = tk.Frame(self, background="blue")
-        self.buttons[0].grid(row=0, column=0, sticky="nsew")
-        self.buttons[1].grid(row=1, column=0, sticky="nsew")
+        self.main = tk.Frame(self)
+        self.buttons[0].grid(row=0, column=1, sticky="nsew")
+        self.buttons[1].grid(row=1, column=1, sticky="nsew")
         self.main.grid(row=2, column=2, columnspan=2, rowspan=6)
 
-        for row in range(8):
+        for row in range(10):
             self.grid_rowconfigure(row, weight=1)
         for col in range(3):
             self.grid_columnconfigure(col, weight=1)
@@ -22,6 +22,6 @@ class Example(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    Example(root).pack(fill="both", expand=True)
+    PassManager(root).pack(fill="both", expand=True)
     root.geometry("800x400")
     root.mainloop()
