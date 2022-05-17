849d57b7263b0f1317929c8130672cc373e6fe8f
pl3081 <petteri.j.lehto@gmail.com>
2022-05-17 11:11:30 +0300

    Updated generator.py
    muutin tiedoston sopivaksi muille moduuleille

--- generator.py
+++ generator.py
@@ -7,31 +7,49 @@ import pyperclip
 
 ## characters to generate password from
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+def main():
+	def generate_random_password(length):
 
-def generate_random_password():
-	## length of password from the user
-	length = int(input("Enter password length: "))
+		## shuffling the characters
+		random.shuffle(characters)
+		
+		## picking random characters from the list
+		password = []
+		for i in range(length):
+			password.append(random.choice(characters))
 
-	## shuffling the characters
-	random.shuffle(characters)
+		## shuffling the resultant password
+		random.shuffle(password)
+
+		## converting the list to string
+		## printing the list
+		print("".join(password))
+		password_label.config(text="".join(password))
+		return "".join(password)
+
+	root = Tk()
+	def generate():
+		generate_random_password(int(password_length.get()))
+
+
+	def copy_to_clipboard():
+		pyperclip.copy(password_label.cget("text"))
+		
 	
-	## picking random characters from the list
-	password = []
-	for i in range(length):
-		password.append(random.choice(characters))
-
-	## shuffling the resultant password
-	random.shuffle(password)
-
-	## converting the list to string
-	## printing the list
-	print("".join(password))
-
-
-	root = tk.
+	password_length = tk.Entry(text="Enter password length: ")
 	
-
-
-
-## invoking the function
-generate_random_password()
+	
+	submit_button = tk.Button(text="submit", command=generate)
+	
+	password_label = Label(text="")
+	
+	copy_password = Button(text="copy to clipboard", command=copy_to_clipboard)
+	
+	
+		
+	password_length.grid(row=0, column=0)
+	submit_button.grid(row=1, column=0)
+	password_label.grid(row=3, column=0)
+	copy_password.grid(row=4, column=0)
+	root.mainloop()
+main()
