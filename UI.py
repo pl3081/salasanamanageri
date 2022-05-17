0f989c17dccc84aea34eb836d42ba0182eca1311
pl3081 <petteri.j.lehto@gmail.com>
2022-05-17 13:01:21 +0300

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,19 +1,19 @@
 import tkinter as tk
 from tkinter import *
 
-root = tk.Tk()
+root = Tk()
 root.title("Password Manager")
 
 PassAndURL = tk.Frame(root)
-PassAndURL.pack()
+PassAndURL.grid()
 
-listbox_URL = tk.Listbox(PassAndURL, height=10, width=48)
-listbox_URL.pack(side=tk.TOP)
+listbox_URL = tk.Listbox(root, height=10, width=48)
+listbox_URL.grid(row=0, column=1, rowspan=2)
 
-Button1= tk.Button(root, text="Button 1", width=48)
-Button1.pack()
+Button1= tk.Button(root, text="Button 1", width=48, height=5)
+Button1.grid(row=0, column=0)
 
-Button2 = tk.Button(root, text="Button 2", width=48)
-Button2.pack()
+Button2 = tk.Button(root, text="Button 2", width=48, height=5)
+Button2.grid(row=1, column=0)
 
 root.mainloop()
